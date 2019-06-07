53ce31bf332c6538a674defe5839ce1fd8aff74b
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-06-07 11:58:14 +0100

    Added css minification using csso to stop svg inline elements from being removed

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,47 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var include = require('gulp-include');
-var concat = require('gulp-concat');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const include = require('gulp-include');
+const concat = require('gulp-concat');
 const child_process = require("child_process");
 const browsersync = require("browser-sync").create();
 const execSync = require('child_process').execSync;
-var sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');
+const argv = require('yargs').argv;
+const autoprefixer = require("autoprefixer"); // Live updates for dev
+const cssnano = require("cssnano"); // CSS Minifier
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+var csso = require('gulp-csso');
 
-var src = {
-    js: ['assets/js/vendor/jquery.js', 'assets/js/**/*.js']
+// PostCSS Plugins
+var postcss_plugins = [
+    autoprefixer(),
+    cssnano({
+        autoprefixer: false,
+        zindex: false
+    })
+];
+
+// Define the publish directory
+if (argv.production === undefined) {
+    var publishdir = '_site'
 }
-var publishdir = '_site'
+else {
+    var publishdir = '_production-site'
+}
+// Sources
+var src = {
+    js: ['assets/js/vendor/jquery.js', 'assets/js/**/*.js'],
+    css: [publishdir + '/assets/css/main.css']
+}
+// Dist
 var dist = {
     all: [publishdir + '/**/*'],
-    js: publishdir + '/static/',
-    vendor: publishdir + '/static/'
+    js: publishdir + '/assets/',
+    vendor: publishdir + '/assets/',
+    css: publishdir + '/assets/css/'
 }
+// Javascript
 function buildJS() {
     return gulp.src(src.js)
         .pipe(sourcemaps.init())
@@ -27,6 +53,25 @@ function buildJS() {
 function compressJS() {
     return gulp.src(dist.js + "app.js")
         .pipe(gulp.dest(dist.js))
+}
+
+// Concatenate & Minifiy CSS
+// CSS is compiled from sass_sources and pushed through autoprefixer(add's cross browser support prefixes --web-kit etc)
+// and then minified using cssnano via the PostCSS API
+function css() {
+    return gulp
+        .src(src.css)
+        .pipe(sourcemaps.init())
+        .pipe(rename({ suffix: ".min" }))
+        // .pipe(postcss(postcss_plugins))
+        .pipe(csso({
+            restructure: false,
+            sourceMap: false,
+            debug: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dist.css))
+        .pipe(browsersync.stream());
 }
 // BrowserSync
 function browserSync(done) {
@@ -45,11 +90,12 @@ function browserSyncReload(done) {
 }
 // Build the Jekyll Site
 function jekyll() {
-    return child_process.spawn("bundle", ["exec", "jekyll", "build", "--profile", "--trace"], { stdio: "inherit" });
-}
-// Build the Jekyll Site
-function jekyll_production() {
-    return child_process.spawn("bundle", ["exec", "jekyll", "build", "--profile", "--trace","--config","_config-production.yml"], { stdio: "inherit" });
+    if (argv.production === undefined){
+        return child_process.spawn("bundle", ["exec", "jekyll", "build", "--profile", "--trace"], { stdio: "inherit" });
+    }
+    else{
+        return child_process.spawn("bundle", ["exec", "jekyll", "build", "--profile", "--trace", "--config", "_config.yml,_config-production.yml", "JEKYLL_ENV=production"], { stdio: "inherit" });
+    }
 }
 // Watch files
 function watchFiles() {
@@ -72,10 +118,8 @@ function watchFiles() {
 
 // Exports/Tasks
 const scripts = gulp.series(buildJS, compressJS);
-const build = gulp.series(jekyll, scripts);
+const build = gulp.series(jekyll, scripts, css);
 exports.js = scripts;
 exports.jekyll = jekyll;
 exports.build = build;
 exports.default = gulp.series(build, gulp.parallel(watchFiles, browserSync));
-exports.build = gulp.series(jekyll, scripts);
-exports.build_production = gulp.series(jekyll_production, scripts);
