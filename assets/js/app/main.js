ec12d1522e223944c08450fa7fba1e07933d0684
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-08-13 13:51:30 +0100

    Added javascript function to open all external links in a new tab

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -134,4 +134,14 @@ $(document).ready(function () {
         });
     }
     // End Slider block instantiation
+
+    // Open External links in a new tab
+    $('a').each(function() {
+       var a = new RegExp('/' + window.location.host + '/');
+       if (!a.test(this.href)) {
+           if($(this).attr("target") != "_self"){
+               $(this).attr("target","_blank");
+           }
+       }
+    });
 });
